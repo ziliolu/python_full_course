fc75ebd38347da3bbffb65e4aaad82585471a55d
Luiza <luzilio.p@gmail.com>
2023-08-21 12:07:40 +0100

    for loops

---
+++ exercises/for_loops.py
@@ -0,0 +1,15 @@
+import time
+
+# for i in range(10):
+#     print(i)
+    
+# for i in range(50, 100, 2):
+#     print(i)
+    
+# for i in "Luiza":
+#     print(i)
+
+for seconds in range(10, 0, -1):
+    print(seconds)
+    time.sleep(1)
+print("Happy new year!")
