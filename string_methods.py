44793314e0e7de4fd2897f71d07f90faa1b8d2a0
Luiza <luzilio.p@gmail.com>
2023-08-21 10:42:43 +0100

    string methods exercises

---
+++ string_methods.py
@@ -0,0 +1,11 @@
+name = "Luiza"
+
+# print(len(name))
+# print(name.find("u"))
+# print(name.capitalize())
+# print(name.upper())
+# print(name.lower())
+# print(name.isdigit())
+# print(name.isalpha())
+# print(name.count("o"))
+print(name.replace("u", "x"))
