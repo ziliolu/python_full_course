54f79084cdd18af6c50286d5884ffbcc702892a0
Luiza <luzilio.p@gmail.com>
2023-08-21 11:02:29 +0100

    math exercises

---
+++ exercises/math_functions.py
@@ -0,0 +1,15 @@
+import math
+
+pi = 3.14
+x = 1 
+y = 2 
+z = 3
+
+print(round(pi))
+print(math.ceil(pi)) #round the number up 
+print(math.floor(pi)) #round the number down
+print(abs(pi)) #absolute value
+print(pow(pi, 2)) #power
+print(math.sqrt(420)) #square root
+print(max(x,y,z))
+print(min(x,y,z))
